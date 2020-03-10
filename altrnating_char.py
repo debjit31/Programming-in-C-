39dba3c1c07fcce02bb7dbb51e829db92e7c0d9a
Debjit <debjitchatterjee31@gmail.com>
2020-03-10 09:44:27 +0530

    Alternating Chararcter

--- altrnating_char.py
+++ altrnating_char.py
@@ -2,10 +2,15 @@ t = int(input())
 while t > 0:
     s = input()
     new = ''
+    c=1
     for i in range(len(s)):
-        if i == 0 or  s[i] != s[i-1]:
+        if i == 0:
             new+=s[i]
-        elif s[i] == s[i-1]:
-            continue
+        elif i!=0 and s[i] != s[i-1]:
+            new+=str(c)
+            c=1
+            new+=s[i]
+        elif s[i] == s[i-1] and new[-1] == s[i]:
+            c+=1
     print(new)
     t-=1
