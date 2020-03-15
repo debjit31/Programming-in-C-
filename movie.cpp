7140118552d6afaea69096c4462d99976ae1f1e2
Debjit <debjitchatterjee31@gmail.com>
2020-03-15 11:41:53 +0530

    Project Commits

--- movie.cpp
+++ movie.cpp
@@ -10,16 +10,13 @@ class Movie{
         string venue;
         int duration;
     public:
-        Movie(string x, string y, string z, string b, int a)
+        void takeInput(string x, string y, string z, string b, int a)
         {
             name=x;
             format=y;
             language=z;
             venue = b;
             duration=a;
-        }
-        Movie(){
-            
         }
         string getName(){
             return name;
@@ -41,8 +38,9 @@ int main(){
     
     Movie arr[10];
     int i = 0;
-    while(1){
-        int ch;
+    int ch;
+    while(1)
+    {
         cout << "Do you want to add movies ?:- Press 1 for Yes  for No. ";
         cin >> ch;
         if (ch == 1)
@@ -50,11 +48,28 @@ int main(){
             cout << "Enter the name of the movie :- ";
             string name;
             getline(cin, name);
+            cin.ignore();
             cout << "Enter the movie format :- ";
             string format;
             getline(cin, format);
-            //arr[i] = m1("Fury", "2D","English","PVR:Avani Mall", 2);
+            cin.ignore();
+            cout << "Enter language :- ";
+            string language;
+            getline(cin, language);
+            cin.ignore();
+            cout << "Enter duration :- ";
+            string duration;
+            getline(cin, duration);
+            cin.ignore();
+            cout << "Enter venue :- ";
+            string venue;
+            getline(cin, venue);
+            cin.ignore();
+            arr[i++].takeInput(name, format,language, venue, duration);
+        }else if(ch==2)
+            break;
         }
+        
     }
 
 
